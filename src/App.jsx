5b4638663c48365868fa3627545cb09ecbc3ebec
EkanshTyagi7 <ekanshtyagi107@gmail.com>
2025-12-22 20:40:35 +0530

    Initial setup: project structure and routing base

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import UserDetails from "./pages/UserDetails";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard/users" element={<Users />} />
+      <Route path="/dashboard/users/:id" element={<UserDetails />} />
+      <Route path="/dashboard/profile" element={<Profile />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default App;
