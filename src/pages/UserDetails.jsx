649c8c78886d0528bac89e88b6ab0775d8f8ad21
EkanshTyagi7 <ekanshtyagi107@gmail.com>
2025-12-23 13:38:20 +0530

    Phase 4: fixed dynamic user links and useParams

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const UserDetails = () => {
+  const {id}=useParams();
   return (
     <div>
       <h2>UserDetail</h2>
+      <p>User is:{id}</p>
     </div>
   )
 }
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Users = () => {
   return (
     <div>
       <h2>Users</h2>
+      <Link to="/dashboard/users/1">User1</Link>
+      <Link to="/dashboard/users/2">User2</Link>
+      <Link to="/dashboard/users/3">User3</Link>
     </div>
   )
 }
